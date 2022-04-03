7e63c62748e869935e146744c66ddfa68bc7b45c
komo <66017773+komo135@users.noreply.github.com>
2022-04-03 13:56:25 +0900

    Update trans.py

--- trans.py
+++ trans.py
@@ -12,31 +12,26 @@ def main():
         pyautogui.hotkey("ctrl", "c")  # Copy text from select text
         try:
             # Make selenium the top screen
-            driver.minimize_window()
+            # driver.minimize_window()
             driver.maximize_window()
         except:
             pass
+
+        # Accepts clipboard values as variables and removes line breaks
+        text = pyperclip.paste()
+        text = re.sub(r"([\w,')''(']+)\r\n|([\w,')''(']+)\n|(\w,')''(']+)\r", r"\1 ", text).replace("  ", " ")
+        text = re.sub(b"\xe2\x80\x90\r\n|\xe2\x80\x90\r|\xe2\x80\x90\n", b"", text.encode()).decode()
+        text = re.sub(r"\r\n|\r|\n", "\n\n", text).replace("\n\n\n", "\n")
+        pyperclip.copy(text)
 
         # Get input fields using xpath
         text_area = driver.find_element(By.XPATH, "//*[@id='panelTranslateText']/div[3]/section[1]/div[3]/div[2]/textarea")
         # Empty input fields
         text_area.clear()
-
-        # Accepts clipboard values as variables and removes line breaks
-        text = pyperclip.paste()
-        text = re.sub(r"([a-zA-Z0-9,]+)\r\n|([a-zA-Z0-9,]+)\n|([a-zA-Z0-9,]+)\r", r"\1 ", text).replace("  ", " ")
-        # To remove "-¥r¥n", encode once, then remove and decode
-        text = re.sub(b"\xe2\x80\x90\r\n|\xe2\x80\x90\r|\xe2\x80\x90\n", b"", text.encode()).decode()
-        text = re.sub(r"\r\n|\r|\n", "\n\n", text).replace("\n\n\n", "\n")
-        pyperclip.copy(text)
-
         text_area.click()
         pyautogui.hotkey("ctrl", "v")
 
-        try:
-            driver.find_element(By.ID, "tabTranslateText").click()
-        except:
-            pass
+        driver.find_element(By.ID, "tabTranslateText").click()
 
     except:
         print("An error has occurred.")
@@ -49,6 +44,7 @@ if __name__ == "__main__":
     driver = webdriver.Chrome()
     driver.get(url)
 
-    keyboard.add_hotkey("ctrl+q", main)
+    keyboard.add_hotkey("無変換", main)
     keyboard.wait("esc")
 
+    driver.quit()
